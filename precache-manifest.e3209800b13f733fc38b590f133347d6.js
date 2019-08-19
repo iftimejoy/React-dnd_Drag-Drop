04adf5e0d579fb68ce0a2dc0b35fa03e5ae69da4
Joyce Iftime <joyceiftime@gmail.com>
2019-08-19 16:46:55 +0300

    Updates

---
+++ precache-manifest.e3209800b13f733fc38b590f133347d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d486209f0d64273ea9f4d04a1130a612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4362c546b39b266160",
+    "url": "/static/js/2.a485b87b.chunk.js"
+  },
+  {
+    "revision": "a82e3d90bf3bcaedaafd",
+    "url": "/static/js/main.d3945cdf.chunk.js"
+  },
+  {
+    "revision": "668d19563c1d480eebd8",
+    "url": "/static/js/runtime~main.3711a48a.js"
+  }
+]);
